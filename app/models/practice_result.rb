53555bbefa36130d937f189232763a28f6557e10
kohei.yamamoto <kohei.yamamoto1012@gmail.com>
2021-08-31 00:53:23 +0900

    fix:scoreの検証を修正

--- app/models/practice_result.rb
+++ app/models/practice_result.rb
@@ -1,10 +1,8 @@
 class PracticeResult < ApplicationRecord
   before_validation :set_name_if_nameless
 
-  #validates :user, presence: true, length: {maximum: 30}
   validates :name, presence: true, length: {maximum: 30}
-  validates :score, presence: true, length: { maximum: 10 }
-
+  validates :score, presence: true
   private
   def set_name_if_nameless
     self.name = "名無し" if name.blank?
--- db/migrate/20210828035805_create_practice_results.rb
+++ db/migrate/20210828035805_create_practice_results.rb
@@ -2,7 +2,7 @@ class CreatePracticeResults < ActiveRecord::Migration[5.2]
   def change
     create_table :practice_results do |t|
       t.string :name, limit: 30
-      t.integer :score, limit: 10, null: false
+      t.integer :score, null: false
 
       t.timestamps
     end
